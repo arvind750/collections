08d7d087c88015b09e0b69f06d4248b4ae8d1388
arvind750 <arvind1221.be21@chitkarauniversity.edu.in>
2025-04-01 14:05:49 +0530

    [Arvind] add: Created Exception Handler to check divison by Zero handling

---
+++ Junit/src/main/java/Basic/MathOperations.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class MathOperations {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
