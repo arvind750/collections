1ca395686695cfc3900f28bf388a1a7e070f588b
arvind750 <arvind1221.be21@chitkarauniversity.edu.in>
2025-04-01 14:26:24 +0530

    [Arvind] add: Added Junit tests for Calculator class

--- Junit/src/test/java/Basic/calculatorTest.java
+++ Junit/src/test/java/Basic/calculatorTest.java
@@ -1,4 +1,30 @@
+package Basic;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class calculatorTest {
-  
+
+calculator c=new calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(3,2,1);
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(1,2,1);
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(3,2,1);
+    }
+
+    @Test
+    void divide() {
+        assertEquals(2,2,4);
+    }
 }
