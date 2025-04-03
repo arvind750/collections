2d10e5b6758e81f5d3ba1dba7e03d6cb276ddbbc
arvind750 <arvind1221.be21@chitkarauniversity.edu.in>
2025-04-03 11:02:51 +0530

    [Arvind] add:  Implemented Main class to demonstrate caching with fibonacci calculations. Showed time optimization by calculating Fibonacci twice, utilizing the cache.

---
+++ annotations/advancedLevelProblems/customCachingSystem/Main.java
@@ -0,0 +1,20 @@
+package advancedLevelProblems.customCachingSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        ExpensiveComputation computation = new ExpensiveComputation();
+
+        System.out.println("Calculating Fibonacci of 20 for the first time...");
+        long startTime = System.nanoTime();
+        int result1 = computation.fibonacci(20);
+        long duration1 = System.nanoTime() - startTime;
+        System.out.println("Result: " + result1 + ", Time: " + duration1 + " ns");
+
+        System.out.println("Calculating Fibonacci of 20 again (should be faster)...");
+
+        startTime = System.nanoTime();
+        int result2 = computation.fibonacci(20);
+        long duration2 = System.nanoTime() - startTime;
+        System.out.println("Result: " + result2 + ", Time: " + duration2 + " ns");
+    }
+}
