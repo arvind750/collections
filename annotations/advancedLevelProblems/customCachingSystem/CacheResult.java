b5b7430022ccb1f3f5eefa79824b49e3b6e4c9a4
arvind750 <arvind1221.be21@chitkarauniversity.edu.in>
2025-04-03 11:00:40 +0530

    [Arvind] add:  Created @CacheResult annotation to mark methods for caching. Applied at runtime to enable caching functionality

---
+++ annotations/advancedLevelProblems/customCachingSystem/CacheResult.java
@@ -0,0 +1,11 @@
+package advancedLevelProblems.customCachingSystem;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // Make it available at runtime
+@Target(ElementType.METHOD) // This annotation will be applied to methods
+public @interface CacheResult {
+}
