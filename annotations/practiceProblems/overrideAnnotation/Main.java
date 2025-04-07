9ca73c8bfd5cf35ac6df4108c90592d60697b0a9
arvind750 <arvind1221.be21@chitkarauniversity.edu.in>
2025-04-07 21:23:21 +0530

    [Arvind] add:    Created Main.java to test Animal and Dog classes with overridden makeSound() method.

---
+++ annotations/practiceProblems/overrideAnnotation/Main.java
@@ -0,0 +1,8 @@
+package practiceProblems.overrideAnnotation;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound(); // Output: Dog barks
+    }
+}
